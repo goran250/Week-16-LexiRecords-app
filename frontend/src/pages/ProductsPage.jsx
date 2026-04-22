5690d60a1bb9b2b6bff3480212403f67912e9688
Göran Rosenberg <GAREN@DESKTOP-5G012SP>
2026-04-23 00:15:14 +0200

    GR

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -1,4 +1,4 @@
-/* Göran was here */
+/* Göran was here again  */
 import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
